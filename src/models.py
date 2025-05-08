323bb03be8c96366ba11e3689189bd5dbb165e54
Alba-cdlg <alba.cdelagrana@icloud.com>
2025-05-08 22:53:25 +0000

    creado el diagrama

--- src/models.py
+++ src/models.py
@@ -1,15 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean, Text 
+from sqlalchemy.orm import relationship, mapped_column, Mapped
+from datetime import datetime
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
 
     def serialize(self):
         return {
@@ -17,3 +19,48 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+    image_url: Mapped[str] = mapped_column(String(255), nullable=False)
+    caption: Mapped[str] = mapped_column(String(500))
+    timestamp: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.utcnow)
+
+    User = relationship("User", backref="posts")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "image_url": self.image_url,
+            "caption": self.caption,
+            "timestamp": self.timestamp.isoformat()
+        }
+
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str] = mapped_column(Text, nullable=False)
+    author_id: Mapped[int] = mapped_column(
+        ForeignKey('user.id'), nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey('post.id'), nullable=False)
+
+    author: Mapped["User"] = relationship("User", backref="comments")
+    post: Mapped["Post"] = relationship("Post", backref="comments")
+
+
+class Follower(db.Model):
+    __tablename__ = 'follower'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_from_id: Mapped[int] = mapped_column(
+        ForeignKey('user.id'), nullable=False)
+    user_to_id: Mapped[int] = mapped_column(
+        ForeignKey('user.id'), nullable=False)
+
+    user_from: Mapped["User"] = relationship(
+        "User", foreign_keys=[user_from_id])
+    user_to: Mapped["User"] = relationship("User", foreign_keys=[user_to_id])
